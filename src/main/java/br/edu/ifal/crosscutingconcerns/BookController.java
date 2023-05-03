a3d402c97563934be51a3dc86d8539f7c3331aca
Leo <leofernandesmo@gmail.com>
2023-05-03 20:04:11 -0300

    Implementação simple de Logging

--- src/main/java/br/edu/ifal/crosscutingconcerns/BookController.java
+++ src/main/java/br/edu/ifal/crosscutingconcerns/BookController.java
@@ -2,6 +2,8 @@ package br.edu.ifal.crosscutingconcerns;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,11 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/books")
 public class BookController {
 
+  Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     private BookRepository bookRepository;
 
     @GetMapping
     public Iterable<Book> findAll() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return bookRepository.findAll();
     }
 
